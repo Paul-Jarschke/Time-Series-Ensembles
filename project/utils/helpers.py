37946ebe543203d1e2bc67049386ab87c3c1ff5b
Paul Richard Jarschke <prj.nw@t-online.de>
2024-02-20 12:38:02 +0100

    commenting, improve metric functions

--- project/utils/helpers.py
+++ project/utils/helpers.py
@@ -1,3 +1,5 @@
+# das kann ignoriert werde, war lediglich für den test des imports gedacht
+
 def add(a,b):
     return(a+b)
 
--- project/utils/metrics.py
+++ project/utils/metrics.py
@@ -2,21 +2,20 @@ import numpy as np
 
 def rmse(predictions, targets):
     """
-    Computes the Root Mean Squared Error (RMSE) between predictions and targets.
+    Calculate the Root Mean Squared Error (RMSE) between predicted values and targets.
 
     Parameters:
-        predictions (numpy.ndarray): Predicted values.
-        targets (numpy.ndarray): Actual values.
+        predictions (array-like): Array containing predicted values.
+        targets (array-like): Array containing target values.
 
     Returns:
         float: RMSE value.
     """
-
     # Convert input arrays to numpy arrays to ensure compatibility
     predictions = np.array(predictions)
     targets = np.array(targets)
 
-    # Calcucalute root mean squared error
+    # Calculate the root mean squared error
     rmse = np.sqrt(((predictions - targets) ** 2).mean())
 
     return rmse
@@ -24,11 +23,11 @@ def rmse(predictions, targets):
 
 def mape(predictions, targets):
     """
-    Calculates the Mean Absolute Percentage Error (MAPE).
+    Calculates the Mean Absolute Percentage Error (MAPE) between predicted values and targets.
 
     Parameters:
         predictions (array-like): Array containing the predicted values.
-        targets (array-like): Array containing the true values.
+        targets (array-like): Array containing target values.
 
     Returns:
         float: MAPE value.
@@ -43,7 +42,7 @@ def mape(predictions, targets):
     # Replace infinite values with NaNs to handle cases where target is zero
     abs_percentage_error[np.isinf(abs_percentage_error)] = np.nan
     
-    # Calculate mean absolute percentage error
+    # Calculate mean absolute percentage error (ignoring nan values)
     mape = np.nanmean(abs_percentage_error) * 100
     
     return mape
