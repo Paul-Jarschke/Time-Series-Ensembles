cebb90c6f5b9faaa5f8015f85f86069c3a33d6df
Paul Richard Jarschke <prj.nw@t-online.de>
2024-02-20 12:20:19 +0100

    data prep and its import working

--- project/data/prepared_data.py
+++ project/data/prepared_data.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import os
+
+# Get the current directory of the script
+script_dir = os.path.dirname(__file__)
+
+# Path to the CSV file
+csv_file_path = os.path.join(script_dir, 'eurusd_df.csv')
 
 # Read data
-df = pd.read_csv('eurusd_df.csv')
+df = pd.read_csv(csv_file_path)
 
 # Drop NaN values for when trading is closed
 data = df.dropna()
