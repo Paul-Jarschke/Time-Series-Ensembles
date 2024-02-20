f54c9981f1bb0ad8afd02e9bd598c263ed670124
Paul Richard Jarschke <prj.nw@t-online.de>
2024-02-20 20:32:04 +0100

    starting modeling process, import error for darts.model ???????

--- project/data/prepared_data.py
+++ project/data/prepared_data.py
@@ -11,10 +11,10 @@ csv_file_path = os.path.join(script_dir, 'eurusd_df.csv')
 df = pd.read_csv(csv_file_path)
 
 # Drop NaN values for when trading is closed
-data = df.dropna()
+df = df.dropna()
 
 # Validation for nan removal
-if data.isnull().sum().sum() == 0:
+if df.isnull().sum().sum() == 0:
     print("There are no NaN or null values in the DataFrame after cleaning.")
 else:
     print("There are still NaN or null values in the DataFrame after cleaning.")
