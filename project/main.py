439eda95821733a7d9a7d5acea1941bd9bdcb007
prichi99 <77013132+prichi99@users.noreply.github.com>
2024-02-22 18:11:55 +0100

    comment for leon

--- project/main.py
+++ project/main.py
@@ -6,3 +6,6 @@ print(add(1,2))
 print(multiply(2,2))
 
 # What's going on here? Delete or is it a placeholder for the pipeline execution file?
+
+
+# erst mal ein placeholder für spätere execution, aber wahrscheinlich sollte die finale analys in nem notbook gemacht werden
