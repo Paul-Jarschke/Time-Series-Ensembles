ea8f33174bc06e23b3d3260076fb8d4419c13cc9
leon-l-247 <leon.loeppert@hotmail.de>
2024-02-23 10:54:21 +0100

    remove double code

--- project/pipeline/pipe3_ensembles.py
+++ project/pipeline/pipe3_ensembles.py
@@ -1,2 +1,22 @@
-from pipeline.pipe2_forecasts import predictions
+#from pipeline.pipe2_forecasts import predictions
+
+# for now CSV import; later: uncomment import from pipe2 (for debugging it takes to long to run full pipe for now)
+
+import pandas as pd
+from utils.weigthing_methods import *
+from utils.metrics import mape, rmse
+
+# Define the file path
+file_path = r'C:\Users\Work\OneDrive\GAU\3. Semester\Statistisches Praktikum\Git\NEW_Ensemble_Techniques_TS_FC\project\interim_results\historical_forecasts.csv'
+
+# Import the CSV file into a DataFrame
+df = pd.read_csv(file_path, index_col='Date', delimiter=';').iloc[:,:-1]
+
+# Display the DataFrame
+print(df)
+
+################
+## Ensembling ##
+################
+
 
