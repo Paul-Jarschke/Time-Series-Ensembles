b57d8acfea7ebc3248b1b1134ca969f4e165958e
prichi99 <77013132+prichi99@users.noreply.github.com>
2024-02-23 11:48:42 +0100

    update

--- project/utils/weigthing_methods.py
+++ project/utils/weigthing_methods.py
@@ -2,6 +2,54 @@ import numpy as np
 
 print('Loading metrics...')
 
+def rmse(predictions, targets):
+    """
+    Calculate the Root Mean Squared Error (RMSE) between predicted values and targets.
+
+    Parameters:
+        predictions (array-like): Array containing predicted values.
+        targets (array-like): Array containing target values.
+
+    Returns:
+        float: RMSE value.
+    """
+    # Convert input arrays to numpy arrays to ensure compatibility
+    predictions = np.array(predictions)
+    targets = np.array(targets)
+
+    # Calculate the root mean squared error
+    rmse = np.sqrt(((predictions - targets) ** 2).mean())
+
+    return rmse
+
+
+def mape(predictions, targets):
+    """
+    Calculates the Mean Absolute Percentage Error (MAPE) between predicted values and targets.
+
+    Parameters:
+        predictions (array-like): Array containing the predicted values.
+        targets (array-like): Array containing target values.
+
+    Returns:
+        float: MAPE value.
+    """
+    # Convert input arrays to numpy arrays to ensure compatibility
+    predictions = np.array(predictions)
+    targets = np.array(targets)
+    
+    # Calculate absolute percentage error
+    abs_percentage_error = np.abs((targets - predictions) / targets)
+    
+    # Replace infinite values with NaNs to handle cases where target is zero
+    abs_percentage_error[np.isinf(abs_percentage_error)] = np.nan
+    
+    # Calculate mean absolute percentage error (ignoring nan values)
+    mape = np.nanmean(abs_percentage_error) * 100
+    
+    return mape
+
+
 def simple_average(data):
     """
     Averages predictions of all models.
@@ -35,7 +83,7 @@ def compute_rmse_weights(data):
     # Extract target 
     target = data['Target']
     
-    # Select columns containing predictions (excluding the first two columns)
+    # Select columns containing predictions (excluding the first column)
     prediction_columns = data.columns[1:]
 
     # Compute RMSE for each model
