162551fb83cf7abca8b1a5f54864449086b40b36
prichi99 <77013132+prichi99@users.noreply.github.com>
2024-02-23 01:51:13 +0100

    univariate pipeline fixed

--- project/pipeline/darts_uni.py
+++ project/pipeline/darts_uni.py
@@ -1,9 +1,6 @@
 # External Modules
-from darts import TimeSeries
-from darts.models import XGBModel, NaiveDrift, NaiveMovingAverage, AutoARIMA
 import pandas as pd
-
-
+from darts.models import XGBModel, NaiveDrift, NaiveMovingAverage, AutoARIMA, RandomForest
 
 # Naive models
 model_naive = NaiveMovingAverage(input_chunk_length=1)
@@ -11,23 +8,53 @@ model_naive_drift = NaiveDrift()
 
 # Not-so naive models
 model_XGB = XGBModel(lags=12)
+model_RF = RandomForest(lags=12)
 model_AutoARIMA = AutoARIMA()
 
-
-models = [model_naive, model_naive_drift, model_XGB, model_AutoARIMA]
-
-
-
+# Create list of models
+univariate_models = [model_naive, model_naive_drift, model_XGB]
 
 
 def hfc_models(models, target_series):
+    """
+    Generate historical forecasts for a given list of models and target time series.
+
+    Args:
+    - models (list): List of models for generating historical forecasts.
+    - target_series (pandas.Series): Target time series for forecasting.
+
+    Returns:
+    - pandas.DataFrame: DataFrame containing historical forecast predictions for each model.
+    """
+
+    # Initiate dictionary for model predictions
+    predictions = {}
 
     for model in models:
+
+        # Extract Model name
+        model_name = type(model).__name__
+        print(f'Training {model_name}:')
+
+        # Generate historical forecasts using the model
         historical_fc = model.historical_forecasts(
             target_series,
-            start = 0.3,
-            forecast_horizon = 1,
-            verbose = True,
-            )
-    
-    
+            start=0.3,
+            forecast_horizon=1,
+            verbose=True,
+            show_warnings=False
+        )
+
+        # Transform darts' TimeSeries to list of values
+        prediction_values = list(historical_fc.values().flatten())
+
+        # Remove last element of historical forecast, since there is no target value for comparison
+        prediction_values.pop()
+        
+        # Using the model's name as the column label
+        predictions[model_name] = prediction_values
+
+    # Creating a DataFrame from the predictions dictionary
+    predictions_df = pd.DataFrame(predictions)
+
+    return predictions_df
