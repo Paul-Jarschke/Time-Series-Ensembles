95108a22cd88eb0cfb5d112038223b84299d14d4
leon-l-247 <leon.loeppert@hotmail.de>
2024-03-07 12:50:00 +0100

    folder structure (separating source code and user files)

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+import src.metrics as metrics
+import src.models as models
+import src.pipeline as pipeline
+import src.simulation as simulation
+import src.utils as utils
---
+++ src/utils/paths.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+print("Loading paths...")
+
+# Package root directory
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+# Data directory and subdirectories
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+SIMDATA_DIR = os.path.join(DATA_DIR, "simulation")
+TESTDATA_DIR = os.path.join(DATA_DIR, "testing")
+
+# User directory and subdirectories
+USER_DIR = os.path.join(ROOT_DIR, "user")
+EXPORT_DIR = os.path.join(USER_DIR, "results")
+
+# Source code directory and subdirectories
+SRC_DIR = os.path.join(ROOT_DIR, "src")
+MODELS_DIR = os.path.join(SRC_DIR, "models")
+METRICS_DIR = os.path.join(SRC_DIR, "metrics")
+
+sys.path.append(ROOT_DIR)
