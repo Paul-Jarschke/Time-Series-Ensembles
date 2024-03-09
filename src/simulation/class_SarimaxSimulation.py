89c1c61bb81e34a1eadf7e570414b40e116be263
leon-l-247 <leon.loeppert@hotmail.de>
2024-03-09 01:58:22 +0100

    fixing data simulation

---
+++ src/simulation/class_SarimaxSimulation.py
@@ -0,0 +1,314 @@
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+
+# Define a custom class for SARIMAX simulation, extending functionality from statsmodels' SARIMAX class.
+class SarimaxSimulation(sm.tsa.SARIMAX):
+    """
+    This module defines a custom class for SARIMAX simulation, extending functionality from statsmodels' SARIMAX class.
+    It also defines the corresponding method for simulation.
+
+    Args:
+        order (tuple):                      ARIMA order.
+        n_simulations (int, optional):      Number of simulations. Defaults to None.
+        start (str, optional):              Start date. Defaults to None.
+        end (str, optional):                End date. Defaults to None.
+        trend (bool, optional):             Flag indicating trend presence. Defaults to False.
+        seasonal_order (tuple, optional):   Seasonal order. Defaults to None.
+        trend_value (float, optional):      Trend value. Defaults to None.
+
+        ar_coefs (list, optional):          AR coefficients. Defaults to None.
+        ma_coefs (list, optional):          MA coefficients. Defaults to None.
+        sar_coefs (list, optional):         Seasonal AR coefficients. Defaults to None.
+        sma_coefs (list, optional):         Seasonal MA coefficients. Defaults to None.
+
+        shock_sigma2 (float, optional):     Shock variance. Defaults to None.
+        bounded (bool, optional):           Flag indicating whether series should be bounded. Defaults to False.
+        lower_bound (float, optional):      Lower bound for bounded series. Defaults to None.
+        upper_bound (float, optional):      Upper bound for bounded series. Defaults to None.
+        exog (DataFrame, optional):         Exogenous variables. Defaults to None.
+        exog_coefs (list, optional):        Exogenous coefficients. Defaults to None.
+
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Raises:
+        ValueError: If `nsimulations` or `start/end` is not defined properly.
+
+    """
+
+    # Initialize models attributes
+    def __init__(self, order, n_simulations=None, start=None, end=None, trend=False,
+                 seasonal_order=None, trend_value=None, ar_coefs=None, ma_coefs=None, sar_coefs=None, sma_coefs=None,
+                 shock_sigma2=None, bounded=False, lower_bound=None, upper_bound=None, exog=None, exog_coefs=None,
+                 *args, **kwargs):
+
+
+
+        # Input validation
+        if not (n_simulations or (start and end)):
+            raise ValueError("Either nsimulations or start/end should be defined.")
+
+        # Convert start and end dates to datetime objects
+        self.start = datetime.strptime(start, "%Y-%m")
+        self.end = datetime.strptime(end, "%Y-%m")
+
+        # Generate an array of months between start and end dates
+        self.months = np.array(
+            pd.period_range(start=self.start, end=self.end, freq="M")
+        )
+
+        # Concatenate exogenous variables to pandas DataFrame if provided as a list of pandas DataFrames
+        if isinstance(exog, list):
+            exog = pd.concat(exog, axis=1)
+        # Otherwise take as it is
+        elif isinstance(exog, (pd.Series, pd.DataFrame)):
+            self.exog = exog
+        elif exog is not None:
+            raise ValueError("Provided exogenous variable(s) must be a (list of) pandas Series.")
+
+        # Determine the number of simulations if it is provided, check for valid input
+        # Otherwise just take the length from start to end (months)
+        self.nsimulations = (
+            n_simulations if n_simulations is not None else len(self.months)
+        )
+        if self.nsimulations != len(self.months):
+            raise ValueError(
+                "'nsimulations' does not equal the number of periods between 'start' and 'end'."
+            )
+
+        # Initialize endogenous variable with empty dataset
+        endog = np.zeros(self.nsimulations)
+
+        # Determine trend choice
+        if trend:
+            trend_choice = "c"
+            if trend_value is None:
+                raise ValueError("You must provide trend value when trend is True.")
+        else:
+            trend_choice = None
+            trend_value = None
+
+        # Adjust coefficients based on provided orders
+        # Seasonal AR
+        if seasonal_order[0] == 0:
+            sar_coefs = None
+        # Seasonal MA
+        if seasonal_order[2] == 0:
+            sma_coefs = None
+
+        if order[0] == 0:
+            ar_coefs = None
+        if order[2] == 0:
+            ma_coefs = None
+
+        # Initialize class attributes
+        self.seed = None
+        self.ma_coefs = ma_coefs
+        self.sar_coefs = sar_coefs
+        self.sma_coefs = sma_coefs
+        self.exog_coefs = exog_coefs
+        self.ar_coefs = ar_coefs
+        self.bounded = bounded
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.trend_binary = trend
+        self.trend_choice = trend_choice
+        self.trend_value = trend_value
+
+        # Create a list of parameters for SARIMAX model
+        param_list = [
+            trend_value,
+            exog_coefs,
+            ar_coefs,
+            ma_coefs,
+            sar_coefs,
+            sma_coefs,
+            shock_sigma2,
+        ]
+
+        # Only add parameter to class parameter list attribute when not None
+        self.params = []
+        for param in param_list:
+            if param is not None:
+                # Input validation
+                if isinstance(param, list):
+                    pass
+                if isinstance(param, int) or isinstance(param, float):
+                    param = [param]
+
+                for p in param:
+                    if not isinstance(p, (int, float)):
+                        raise ValueError(f"Provided parameter {param} must be int or float.")
+                self.params += param
+
+        # Initialize statsmodels SARIMAX model with specified parameters
+        super().__init__(
+            endog=endog,
+            order=order,
+            seasonal_order=seasonal_order,
+            trend=trend_choice,
+            exog=exog,
+            *args,
+            **kwargs,
+        )
+
+        # Validate parameter count
+        if len(self.params) != len(self.param_names):
+            raise ValueError(
+                f"Parameters are not correctly defined. Expected {len(self.param_names)} parameters, but provided "
+                f"{len(self.params)}. Check that you have defined all of {self.param_names}."
+            )
+
+    # Define simulation method to extend functionality of parent class
+    def simulate(self, seed=None, *args, **kwargs):
+        """
+        Simulate method to generate time series data.
+
+        Args:
+            seed (int, optional): Random seed. Defaults to None.
+
+        Returns:
+            SimulatedSeries: Simulated time series data.
+
+        """
+        # Calculate seed value for reproducibility
+        seed = (
+                987
+                + seed
+                + int(np.sum(self.params))
+                + int(np.sum(self.seasonal_order))
+                + int(np.sum(self.order))
+                + int(self.trend_binary)
+        )
+        np.random.seed(seed)
+        self.seed = seed
+
+        # Generate random shocks (error term) coming from a normal distribution
+        # Mean zero and defined error variance (last position in params)
+        shocks = np.random.normal(
+            size=self.nsimulations, loc=0, scale=np.sqrt(self.params[-1])
+        )
+
+        # Simulate series using parent class method (from statsmodels)
+        series = super().simulate(
+            params=self.params,
+            nsimulations=self.nsimulations,
+            random_state=seed,
+            measurement_shocks=shocks,
+            exog=self.exog  # ??
+        )
+
+        # Apply bounding if specified (this just scales and moves the series)
+        # For more realistic series that fall between certain intervals (e.g. are positive etc.)
+        if self.bounded:
+            a, b = self.lower_bound, self.upper_bound
+            series = ((series - min(series)) / (max(series) - min(series))) * (
+                    b - a
+            ) + a
+
+        # Create pandas Series object from simulated ndarray series with appropriate index
+
+
+        # Add all attributes from parent class to series
+
+        # Assign our custom SimulatedSeries class to series (defined below this class)
+        series = SimulatedSeries(
+            series,
+            seed=seed,
+            exog=self.exog,
+            order=self.order,
+            seasonal_order=self.seasonal_order,
+            months=self.months,
+            trend=self.trend_binary,
+        )
+
+        return series
+
+
+# Define a custom Series class for resulting Simulated Series that simulated data is assigned to (inherits from
+# pd.Series; e.g., for plotting)
+class SimulatedSeries(pd.Series):
+    """
+    Custom class for Simulated Series. Inherits from pandas' Series class and adds attributes for simulation details.
+    Adjusts pandas .plot() method for our purposes.
+
+    Args:
+        series (pd.Series): Simulated series.
+        seed (int): Random seed.
+        order (tuple): ARIMA order.
+        seasonal_order (tuple): Seasonal order.
+        trend (bool): Flag indicating trend presence.
+        exog (DataFrame): Exogenous variables.
+
+    """
+
+    def __init__(self, series, seed, order, seasonal_order, trend, exog, months, *args, **kwargs):
+
+        # Assign attributes from parent class
+        super().__init__(data=series, index=months, name="simulated_data")
+
+        # Initialize series attributes
+        self.index.name = "Month"
+        self.seed = seed
+        self.series = series
+        self.months = super().index
+        self.month_strings = [str(month) for month in self.months]
+        self.seasonal_order = seasonal_order
+        self.order = order
+        self.seasonal_label = (
+            f"${self.seasonal_order[:3]}_{{{self.seasonal_order[3]}}}$"
+        )
+
+        self.plot_title = "ARIMA"
+        self.exog = exog
+        self.trend = trend
+        # Determine the number of exogenous variables
+        self.n_exog = 0 if self.exog is None else self.exog.shape[1]
+
+    # Plot method for visualizing the simulated series/overwrite pandas plotting method
+    def plot(self, title="Simulation", *args, **kwargs):
+        """
+        Plot method for visualizing the simulated series.
+
+        Args:
+            title (str, optional): Plot title. Defaults to "Simulation".
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        """
+
+        # Adjust plot title based on model components
+        self.plot_title += f"{'X' if self.n_exog > 0 else ''}{self.order}"
+        if not all(x == 0 for x in self.seasonal_order[:3]):
+            self.plot_title = f"S{self.plot_title}{self.seasonal_label}"
+
+        if self.trend:
+            self.plot_title = self.plot_title + " with trend"
+            if self.n_exog > 0:
+                self.plot_title = (
+                        self.plot_title + f" and {self.n_exog} exogenous variables"
+                )
+        elif self.n_exog > 0:
+            self.plot_title = (
+                    self.plot_title + f" with {self.n_exog} exogenous variables"
+            )
+
+        # Generate the plot
+        plt.figure(figsize=(6, 5))
+        plt.suptitle(title, size=16, y=0.98)
+        plt.title(f"{self.plot_title}", size=14)
+        freq = 12
+        xticks_positions = np.arange(0, len(self.series), freq * 2)
+        plt.xticks(ticks=xticks_positions, rotation=45)
+        plt.xlabel("Time")
+        plt.ylabel("Price")
+        plt.plot(
+            self.month_strings, self.series, label="Simulated Data", *args, **kwargs
+        )
+        plt.show()
+        self.plot_title = "ARIMA"  # Reset title
--- src/simulation/data_simulation_sarimax.py
+++ src/simulation/data_simulation_sarimax.py
@@ -1,21 +1,17 @@
 # Import modules
-import os
-
 import numpy as np
 import pandas as pd
 
-from SARIMAX_sim_class import SARIMAX_Simulation
+from src.utils.paths import *
+from src.simulation.class_SarimaxSimulation import SarimaxSimulation
 
 # Set number of periods
-start = "01/2004"
-end = "12/2023"
+start = "2004-01"
+end = "2023-12"
 
 # Alternative (has to be specified as arg in function call !)
 # years = 10
-# nsimulations = years * 12 
-
-# Directory to store simulated data
-sim_dir = r'/data/simulated/'
+# nsimulations = years * 12
 
 # Set Seed
 seed = 42
@@ -24,75 +20,72 @@ seed = 42
 ## Exogeneous Variable 1 ##
 ###########################
 
-exo1_model = SARIMAX_Simulation(
+exo1_model = SarimaxSimulation(
     start=start, end=end,
-    order=(1, 1, 3),                            # ARIMA (p,d,q)
-    seasonal_order=(2, 0, 0, 12),               # Seasonal Component (P,D,Q,m)
+    order=(1, 1, 3),  # ARIMA (p,d,q)
+    seasonal_order=(2, 0, 0, 12),  # Seasonal Component (P,D,Q,m)
     trend=True, trend_value=0.94,
-    ar_coefs=[0.2], ma_coefs=[0.4, 0.25, 0.1], 
+    ar_coefs=[0.2], ma_coefs=[0.4, 0.25, 0.1],
     sar_coefs=[0.05, 0.01], sma_coefs=[],
-    shock_sigma2=100,                           # shock/error term variance
+    shock_sigma2=100,  # shock/error term variance
     bounded=True, lower_bound=200, upper_bound=500
 )
 
 exo1_ts = exo1_model.simulate(seed)
 
-
 ###########################
 ## Exogeneous Variable 2 ##
 ###########################
 
-exo2_model = SARIMAX_Simulation(
+exo2_model = SarimaxSimulation(
     start=start, end=end,
-    order=(2, 1, 4),                                        # ARIMA (p,d,q)
-    seasonal_order=(1, 0, 0, 12),                           # Seasonal Component (P,D,Q,m)
+    order=(2, 1, 4),  # ARIMA (p,d,q)
+    seasonal_order=(1, 0, 0, 12),  # Seasonal Component (P,D,Q,m)
     trend=False, trend_value=-0.1,
-    ar_coefs=[0.7, 0.01], ma_coefs=[0.5, 0.2, 0.1, -0.1], 
+    ar_coefs=[0.7, 0.01], ma_coefs=[0.5, 0.2, 0.1, -0.1],
     sar_coefs=[0.2], sma_coefs=[],
-    shock_sigma2=1,                                         # shock/error term variance
+    shock_sigma2=1,  # shock/error term variance
     bounded=True, lower_bound=40, upper_bound=80
 )
 
 exo2_ts = exo2_model.simulate(seed)
 
-
 ######################
 ## Exogeneous Var 3 ##
 ######################
 
-exo3_model = SARIMAX_Simulation(
+exo3_model = SarimaxSimulation(
     start=start, end=end,
-    order=(3, 1, 1),                                # ARIMA (p,d,q)
-    seasonal_order=(2, 0, 1, 12),                   # Seasonal Component (P,D,Q,m)
+    order=(3, 1, 1),  # ARIMA (p,d,q)
+    seasonal_order=(2, 0, 1, 12),  # Seasonal Component (P,D,Q,m)
     trend=True, trend_value=0.1,
-    ar_coefs=[0.15, -0.01, -0.01], ma_coefs=[0.35], 
+    ar_coefs=[0.15, -0.01, -0.01], ma_coefs=[0.35],
     sar_coefs=[0.3, 0.2], sma_coefs=[-0.2],
-    shock_sigma2=4,                             # shock/error term variance
+    shock_sigma2=4,  # shock/error term variance
     bounded=True, lower_bound=900, upper_bound=950
 )
 
 exo3_ts = exo3_model.simulate(seed)
 
-
 ###########################
 ##  Endogenous Variable  ##
 ###########################
 
-# Define exogenous variables and respective coeffients
+# Define exogenous variables and respective coefficients
 exo_ts = [exo1_ts, exo2_ts, exo3_ts]
 exo_coefs = [0.3, 0.15, -0.1]
 
-endo_model = SARIMAX_Simulation(
-        start = start, end = end,
-        exog = exo_ts,
-        exog_coefs = exo_coefs,
-        order = (1, 1, 5), 
-        seasonal_order = (4, 0, 0, 6), 
-        trend = True, trend_value = 0.1, 
-        ar_coefs = [0.4], ma_coefs = [0.15, 0.05, -0.005, -0.01, -0.01], 
-        sar_coefs = [0.1, -0.1, 0.05, -0.01], sma_coefs = [], 
-        shock_sigma2 = 5,
-        bounded = True, lower_bound = 40, upper_bound = 90)
+endo_model = SarimaxSimulation(
+    start=start, end=end,
+    exog=exo_ts,
+    exog_coefs=exo_coefs,
+    order=(1, 1, 5),
+    seasonal_order=(4, 0, 0, 6),
+    trend=True, trend_value=0.1,
+    ar_coefs=[0.4], ma_coefs=[0.15, 0.05, -0.005, -0.01, -0.01],
+    sar_coefs=[0.1, -0.1, 0.05, -0.01], sma_coefs=[],
+    shock_sigma2=5,
+    bounded=True, lower_bound=40, upper_bound=90)
 
 endo_ts = endo_model.simulate(seed)
 
@@ -112,35 +105,23 @@ std_dev_range = (0.2, 0.9)
 # Create a list to store the noisy time series
 noisy_series = []
 
-# Loop through each original time series
-for ts in series:
+##########################################
+##  Set up DataFrame with noisy series  ##
+##########################################
 
+noisy_df = pd.DataFrame(series)
+for ts in series:
     # Randomly draw mean and standard deviation
     mean = np.random.uniform(*mean_range)
     std_dev = np.random.uniform(*std_dev_range)
-    
+
     # Generate random noise
     noise = np.random.normal(mean, std_dev, len(ts))
-    
+
     # Add noise to the original time series
     noisy_ts = ts + noise
-    
-    # Append the noisy time series to the list
-    noisy_series.append(noisy_ts)
 
-
-
-##############################
-##  Set up noise DataFrame  ##
-##############################
-
-# Create empty dataframe
-noisy_df = pd.DataFrame()
-
-for ts in noisy_series:
-    
-    # Concatenate the series to dataframe
-    noisy_df = pd.concat([noisy_df, ts], axis=1)
+    noisy_df = pd.concat([noisy_df, noisy_ts], axis=1)
 
 # Rename Columns
 noisy_df.columns = ['y', "x1", "x2", "x3"]
@@ -150,14 +131,12 @@ noisy_df.index.name = "Date"
 ### noisy_df.insert(0, 'Date', endo_ts.index.to_series())
 
 # Format periodic 'date' index to ISO YYYY-MM-DD format
-noisy_df.index = pd.PeriodIndex(noisy_df.index, freq = "M").to_timestamp().strftime('%Y-%m-%d')
-
-
+noisy_df.index = pd.PeriodIndex(noisy_df.index, freq="M").to_timestamp().strftime('%Y-%m-%d')
 
 ##########################
 ##  Export Data as CSV  ##
 ##########################
 
 ### die Daten, die wir final verwenden sollten wir zur Transparenz trotz pipeline exportieren
-filename = f'noisy_simdata.csv'
-noisy_df.to_csv(os.path.join(sim_dir, filename))
+filename = f'noisy_simdata_neu.csv'
+noisy_df.to_csv(os.path.join(SIMDATA_DIR, filename))
